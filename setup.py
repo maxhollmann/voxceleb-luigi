8ca58495add65202b751fe4e2b75da4daeec93d8
Max Hollmann <maxhollmann@gmail.com>
2018-04-14 20:48:22 +0200

    Add readme to pypi package

--- setup.py
+++ setup.py
@@ -1,9 +1,17 @@
 from setuptools import setup
+from pathlib import Path
+
+
+# Get the long description from the README file
+with (Path(__file__).resolve().parent / 'README.md').open(encoding='utf-8') as f:
+    long_description = f.read()
 
 setup(
     name='voxceleb_luigi',
-    version='0.1.0',
+    version='0.1.2',
     description='Luigi pipeline to download VoxCeleb audio from YouTube and extract speaker segments',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     url='https://github.com/maxhollmann/voxceleb-luigi',
     author='Max Hollmann',
     license='MIT',
